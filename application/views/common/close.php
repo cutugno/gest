4ca33d82deef3a0ac91ecd061440fb6557e53efe
Alessandro Nocella <sberz666@gmail.com>
2016-12-28 21:30:24 +0100

    aggiunti asset tablesorter in common view

--- application/views/common/close.php
+++ application/views/common/close.php
@@ -4,6 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
 <link rel="stylesheet" href="<?php echo site_url('assets/js/bootstrap.min.js'); ?>">
+<link rel="stylesheet" href="<?php echo site_url('assets/js/jquery.tablesorter.js'); ?>">
+<link rel="stylesheet" href="<?php echo site_url('assets/js/jquery.tablesorter.pager.js'); ?>">
+<link rel="stylesheet" href="<?php echo site_url('assets/js/jquery.tablesorter.widgets.js'); ?>">
+<link rel="stylesheet" href="<?php echo site_url('assets/js/tablesorter-parser-input-select.js'); ?>">
 
 </body>
 </html>
